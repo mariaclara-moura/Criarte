2ce2407c1d5461124e6d6ecec8991ea7e3dcb76d
Maria Clara Albuquerque Moura <amouramariaclara@gmail.com>
2024-05-22 15:01:44 -0300

    Resolve bug de cor do botão

--- Criarte/Criarte/ContentView.swift
+++ Criarte/Criarte/ContentView.swift
@@ -1,354 +1,204 @@
 import SwiftUI
 import Foundation
 
+extension Attribute {
+    static var start: [Attribute] {
+        [
+                Attribute(
+                    name: "Expressividade",
+                    position: 0,
+                    weight: 8
+                ),
+                Attribute(
+                    name: "Autenticidade",
+                    position: 0,
+                    weight: 6
+                ),
+                Attribute(
+                    name: "Complexidade",
+                    position: 0,
+                    weight: 1
+                ),
+                Attribute(
+                    name: "Inovação",
+                    position: 0,
+                    weight: 2
+                ),
+                Attribute(
+                    name: "Impacto",
+                    position: 0,
+                    weight: 4
+                ),
+                Attribute(
+                    name: "Coerência",
+                    position: 0,
+                    weight: 3
+                ),
+                Attribute(
+                    name: "Curiosidade",
+                    position: 0,
+                    weight: 5
+                ),
+                Attribute(
+                    name: "Experimentação",
+                    position: 0,
+                    weight: 7
+                )
+        ]
+    }
+}
 struct ContentView: View {
-    @State var attributes = [
-        Attribute(
-            name: "Expressividade",
-            position: 0,
-            weight: 8
-        ),
-        Attribute(
-            name: "Autenticidade",
-            position: 0,
-            weight: 6
-        ),
-        Attribute(
-            name: "Complexidade",
-            position: 0,
-            weight: 1
-        ),
-        Attribute(
-            name: "Inovação",
-            position: 0,
-            weight: 2
-        ),
-        Attribute(
-            name: "Impacto",
-            position: 0,
-            weight: 4
-        ),
-        Attribute(
-            name: "Coerência",
-            position: 0,
-            weight: 3
-        ),
-        Attribute(
-            name: "Curiosidade",
-            position: 0,
-            weight: 5
-        ),
-        Attribute(
-            name: "Experimentação",
-            position: 0,
-            weight: 7
-        )
-    ]
+    @State var attributes = Attribute.start
     @State var allSlidersMoved = false
     @State var result: Double?
     @State var showAlert = false
     @State var showInfo = false
     @State var selectedArt = allArts.randomElement()!
     @State var resetButton = false
-    
+    @State var scrollToTop = false
+
     var body: some View {
         ZStack {
-            Color(
-                .pinkBackground
-            )
-            .edgesIgnoringSafeArea(
-                .all
-            )
-            
+            Color(.pinkBackground )
+            .edgesIgnoringSafeArea(.all)
             VStack {
                 ZStack {
                     Ellipse()
-                        .frame(
-                            width: 492,
-                            height: 304
-                        )
-                        .foregroundColor(
-                            Color.darkBrown
-                        )
-                    HStack(
-                        alignment: .top
-                    ) {
-                        Text(
-                            "Criarte"
-                        )
-                        .font(
-                            .pottaOne(
-                                size: 48.0
-                            )
-                        )
-                        .foregroundColor(
-                            Color.orangeTitle
-                        )
-                        Button(action: {
-                            showInfo = true
-                        }) {
-                            Image(
-                                systemName: "info.circle"
-                            )
-                            .foregroundColor(
-                                Color(
-                                    .orangeTitle
-                                )
-                            )
+                        .frame( width: 492, height: 304)
+                        .foregroundColor(Color.darkBrown)
+                    HStack(alignment: .top) {
+                        Text("Criarte")
+                        .font(.pottaOne(size: 48.0))
+                        .foregroundColor(Color.orangeTitle)
+                        Button(action: {showInfo = true}) {
+                            Image(systemName: "info.circle")
+                            .foregroundColor(Color(.orangeTitle))
                         }
                     }
-                    .padding(
-                        .top,
-                        150.0
-                    )
-                    .padding(
-                        .leading,
-                        20.0
-                    )
+                    .padding(.top, 150.0)
+                    .padding(.leading,20.0)
                 }
-                .edgesIgnoringSafeArea(
-                    .top
-                )
-                .padding(
-                    .top,
-                    -130.0
-                )
-                
+                .edgesIgnoringSafeArea(.top)
+                .padding( .top,-130.0)
                 ZStack {
-                    Text(
-                        "\(selectedArt.name), \(selectedArt.author)"
-                    )
-                    .font(
-                        .poppinsRegular(
-                            size: 12.0
-                        )
-                    )
-                    .frame(width: 350.0)
-                    .rotationEffect(
-                        .degrees(
-                            270
-                        )
-                    )
-                    .foregroundColor(
-                        Color.darkBrown
-                    )
-                    .offset(
-                        x: -140
-                    )
+                    Text( "\(selectedArt.name), \(selectedArt.author)")
+                    .font(.poppinsRegular(size: 12.0))
+                    .frame( width: 350.0)
+                    .rotationEffect(.degrees(270))
+                    .foregroundColor(Color.darkBrown)
+                    .offset(x: -140)
                     ZStack {
                         Rectangle()
-                            .frame(
-                                width: 250,
-                                height: 305
-                            )
-                            .foregroundColor(
-                                Color(
-                                    .darkBrown
-                                )
-                            )
-                        Image(
-                            selectedArt.archive
-                        )
+                            .frame( width: 250,height: 305)
+                            .foregroundColor( Color(.darkBrown))
+                        Image(selectedArt.archive)
                         .resizable()
                         .scaledToFit()
-                        .frame(
-                            width: 218.0,
-                            height: 265.0
-                        )
+                        .frame( width: 218.0,height: 265.0)
                     }
                 }
-                .padding(
-                    .top,
-                    -30.0
-                )
-                
-                Text(
-                    "Como você percebe a presença de..."
-                )
-                .font(
-                    .poppinsSemiBold(
-                        size: 20.0
-                    )
-                )
-                .foregroundColor(
-                    Color(
-                        .darkBrown
-                    )
-                )
-                .multilineTextAlignment(
-                    .center
-                )
-                .frame(
-                    width: 270.0,
-                    height: 60.0
-                )
-                
-                ScrollView(
-                    .vertical,
-                    showsIndicators: true
-                ) {
-                    VStack(
-                        spacing: 2
-                    ) {
-                        ForEach(
-                            attributes.indices,
-                            id: \.self
-                        ) { index in
-                            let attribute = attributes[index]
-                            HStack {
-                                VStack(
-                                    alignment: .leading
-                                ) {
-                                    Text(
-                                        attribute.name
-                                    )
-                                    .font(
-                                        .poppinsRegular(
-                                            size: 16.0
-                                        )
-                                    ).padding(
-                                        .leading,
-                                        2.0
-                                    )
-                                    .foregroundColor(
-                                        Color(
-                                            .darkBrown
-                                        )
-                                    )
-                                    Slider(
-                                        value: $attributes[index].position,
-                                        in: 0...10,
-                                        step: 1
-                                    )
-                                    .accentColor(
-                                        Color(
-                                            .orangeButtomOn
-                                        )
-                                    )
-                                    .frame(
-                                        width: 280,
-                                        height: 20.0
-                                    )
-                                    .onChange(
-                                        of: attributes[index].position
-                                    ) {
-                                        attributes[index].moved = resetButton ? false : true
-                                        updateButtonColor()
+                .padding(.top, -30.0)
+                Text("Como você percebe a presença de...")
+                .font( .poppinsSemiBold(size: 20.0))
+                .foregroundColor(Color(.darkBrown))
+                .multilineTextAlignment(.center)
+                .frame(width: 270.0,height: 60.0)
+                ScrollViewReader { value in
+                    ScrollView( .vertical, showsIndicators: true ) {
+                        VStack(spacing: 2) {
+                            ForEach( attributes.indices, id: \.self) { index in
+                                let attribute = attributes[index]
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        Text(attribute.name)
+                                        .font(.poppinsRegular(size: 16.0))
+                                        .padding(.leading,2.0)
+                                        .foregroundColor(Color( .darkBrown ) )
+                                        Slider( value: $attributes[index].position, in: 0...10, step: 1)
+                                        .accentColor( Color(.orangeButtomOn))
+                                        .frame( width: 280, height: 20.0)
+                                        .onChange(of: attributes[index].position) {
+                                            print("antes resetbutton: \(resetButton), \(attributes[index].name).moved: \(attributes[index].moved)")
+                                            attributes[index].moved = (
+                                                resetButton &&
+                                                attributes[index].moved == .moved &&
+                                                !attributes[index].didChange
+                                            ) ? .idle : .moved
+                                            attributes[index].didChange = true
+                                            print("depois resetbutton: \(resetButton), \(attributes[index].name).moved: \(attributes[index].moved)")
+                                            updateButtonColor()
+                                        }
                                     }
+                                    Spacer()
                                 }
-                                Spacer()
+                                .padding(.leading,20.0)
                             }
-                            .padding(
-                                .leading,
-                                20.0
-                            )
+                            .frame( width: 330,height: 40)
+                            .padding()
                         }
-                        .frame(
-                            width: 330,
-                            height: 40
-                        )
-                        .padding()
+                    }
+                    .onChange( of: scrollToTop) {
+                        if scrollToTop {
+                            withAnimation {
+                                value.scrollTo( 0, anchor: .top )
+                            }
+                            scrollToTop = false
+                        }
                     }
                 }
                 
                 Button(action: {
                     processResult()
+                    scrollToTop = true
                 }) {
-                    Text(
-                        "Criartivar"
-                    )
-                    .font(
-                        .poppinsSemiBold(
-                            size: 24.0
-                        )
-                    )
-                    .foregroundColor(
-                        allSlidersMoved ? Color(
-                            .darkBrown
-                        ) : Color(
-                            .lightBrown
-                        )
-                    )
+                    Text("Criartivar")
+                    .font(.poppinsSemiBold( size: 24.0) )
+                    .foregroundColor( allSlidersMoved ? Color( .darkBrown ) : Color( .lightBrown ))
                     .padding()
-                    .frame(
-                        width: 200,
-                        height: 45
-                    )
-                    .background(
-                        allSlidersMoved ? Color(
-                            .orangeButtomOn
-                        ) : Color(
-                            .orangeButtomOff
-                        )
-                    )
-                    .cornerRadius(
-                        16
-                    )
+                    .frame( width: 200,height: 45)
+                    .background(allSlidersMoved ? Color( .orangeButtomOn ) : Color( .orangeButtomOff ) )
+                    .cornerRadius(16)
                 }
-                .padding(
-                    .top,
-                    20
-                )
-                .disabled(
-                    !allSlidersMoved
-                )
-                
+                .padding( .top,20)
+                .disabled( !allSlidersMoved)
             }
             if showAlert {
-                PopUp(
-                    showAlert: $showAlert,
-                    result: $result,
-                    resetAttributes: resetAttributes
-                )
+                PopUp(showAlert: $showAlert,result: $result, resetAttributes: resetAttributes)
             }
             if showInfo {
-                Info(
-                    showInfo: $showInfo
-                )
+                Info(showInfo: $showInfo)
             }
         }
-        .edgesIgnoringSafeArea(
-            .top
-        )
+        .edgesIgnoringSafeArea( .top )
     }
     
     func updateButtonColor() {
-        var allTrue = true
-        for attribute in attributes {
-            if !attribute.moved {
-                allTrue = false
-                break
-            }
-        }
+        var allTrue = attributes.allSatisfy { $0.moved == .moved }
         allSlidersMoved = allTrue
+        print("alltrue \(allTrue)")
+        if allTrue {
+            resetButton = false
+            print("liberou geral")
+        }
     }
     
     func resetAttributes() {
         for index in attributes.indices {
             attributes[index].position = 0
-            attributes[index].moved = false
+            attributes[index].didChange = false
         }
         selectedArt = allArts.randomElement()!
         allSlidersMoved = false
         resetButton = true
+        print("bloqueou geral")
     }
     
     func processResult() {
         let totalWeight = 36.0
-        let weightedSum = attributes.reduce(
-            0.0
-        ) {
-            sum,
-            attribute in
-            sum + (
-                attribute.position * Double(
-                    attribute.weight
-                )
-            )
+        let weightedSum = attributes.reduce(0.0) {
+            sum, attribute in sum + ( attribute.position * Double(attribute.weight))
         }
-        result = (
-            weightedSum / totalWeight
-        ) * 10
+        result = (weightedSum / totalWeight) * 10
         showAlert = true
     }
     
@@ -359,8 +209,15 @@ struct Attribute: Identifiable {
     let name: String
     var position: Double
     var weight: Int
-    var moved = false
+    var moved = State.idle
+    var didChange = false
+
+    enum State {
+        case idle
+        case moved
+    }
 }
+
 
 
 #Preview {
--- Criarte/Criarte/PopUp.swift
+++ Criarte/Criarte/PopUp.swift
@@ -12,194 +12,87 @@ struct PopUp: View {
     var resetAttributes: () -> Void
     
     var body: some View {
-        ZStack{
-            Color(.black).opacity(0.3)
-            VStack(
-                alignment: .center
-            ) {
-                VStack {
-                    HStack {
-                        Spacer()
-                        Button(action:{
-                            showAlert = false
-                        }) {
-                            Image(
-                                systemName: "xmark"
-                            )
-                            .foregroundColor(
-                                Color(
-                                    .orangeButtomOn
-                                )
-                            )
-                            .padding(
-                                [
-                                    .bottom,
-                                    .trailing
-                                ],
-                                10.0
-                            )
-                            .font(
-                                .system(
-                                    size: 30
-                                )
-                            )
-                        }
+        ZStack {
+            if showAlert {
+                Color.black.opacity(0.3)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        showAlert = false
                     }
-                    .frame(
-                        width: 300.0
-                    )
-                    VStack(
-                        spacing: -16.0
-                    ) {
-                        Text(
-                            "Você absorveu"
-                        )
-                        .foregroundColor(
-                            Color(
-                                .darkBrown
-                            )
-                        )
-                        .padding(
-                            .bottom,
-                            20
-                        )                    .font(
-                            .poppinsBold(
-                                size: 36.0
-                            )
-                        )
+
+                VStack(alignment: .center) {
+                    VStack {
                         HStack {
-                            Text("\(Int(result ?? 0))%"
-                            )
-                            .foregroundColor(
-                                Color(
-                                    .orangeButtomOn
-                                )
-                            )
-                            .padding(
-                                .bottom,
-                                20
-                            ) .font(
-                                .poppinsBold(
-                                    size: 40.0
-                                )
-                            )
-                            Text(
-                                "de"
-                            )
-                            .foregroundColor(
-                                Color(
-                                    .darkBrown
-                                )
-                            )
-                            .multilineTextAlignment(
-                                .center
-                            )
-                            .padding(
-                                .bottom,
-                                20
-                            ) .font(
-                                .poppinsBold(
-                                    size: 36.0
-                                )
-                            )
+                            Spacer()
+                            Button(action:{
+                                showAlert = false
+                            }) {
+                                Image(systemName: "xmark")
+                                    .foregroundColor(Color.orangeButtomOn)
+                                    .padding([.bottom, .trailing], 10.0)
+                                    .font(.system(size: 30))
+                            }
                         }
-                        Text(
-                            "criatividade!"
-                        )
-                        .foregroundColor(
-                            Color(
-                                .darkBrown
-                            )
-                        )
-                        .multilineTextAlignment(
-                            .center
-                        )
-                        .padding(
-                            .bottom,
-                            20
-                        )                    .font(
-                            .poppinsBold(
-                                size: 36.0
-                            )
-                        )
+                        .frame(width: 300.0)
+                        
+                        VStack(spacing: -16.0) {
+                            Text("Você absorveu")
+                                .foregroundColor(Color.darkBrown)
+                                .padding(.bottom, 20)
+                                .font(.poppinsBold(size: 36.0))
+                            
+                            HStack {
+                                Text("\(Int(result ?? 0))%")
+                                    .foregroundColor(Color.orangeButtomOn)
+                                    .padding(.bottom, 20)
+                                    .font(.poppinsBold(size: 40.0))
+                                Text("de")
+                                    .foregroundColor(Color.darkBrown)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.bottom, 20)
+                                    .font(.poppinsBold(size: 36.0))
+                            }
+                            
+                            Text("criatividade!")
+                                .foregroundColor(Color.darkBrown)
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom, 20)
+                                .font(.poppinsBold(size: 36.0))
+                        }
                     }
-                }
-                Button(action: {
-                    resetAttributes()
-                    showAlert = false
                     
-                }) {
-                    Text(
-                        "Nova avaliação"
-                    )
-                    .font(
-                        .poppinsSemiBold(
-                            size: 20.0
-                        )
-                    )
-                    .foregroundColor(
-                        Color(
-                            .darkBrown
-                        )
-                    )
-                    .frame(
-                        width: 230,
-                        height: 60
-                    )
-                    .background(
-                        Color(
-                            .orangeButtomOn
-                        )
-                    )
-                    .cornerRadius(
-                        10
-                    )
+                    Button(action: {
+                        resetAttributes()
+                        showAlert = false
+                    }) {
+                        Text("Nova avaliação")
+                            .font(.poppinsSemiBold(size: 20.0))
+                            .foregroundColor(Color.darkBrown)
+                            .frame(width: 230, height: 60)
+                            .background(Color.orangeButtomOn)
+                            .cornerRadius(10)
+                    }
+                    .padding(.bottom, 32.0)
                 }
-                .padding(
-                    .bottom,
-                    32.0
+                .frame(width: 355, height: 450)
+                .background(Color.pinkBackground)
+                .cornerRadius(20)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.orangeButtomOn, lineWidth: 4)
                 )
+                .shadow(radius: 10)
+                .transition(.opacity)
+                .animation(.easeInOut(duration: 0.3), value: showAlert)
             }
-            .frame(
-                width: 355,
-                height: 450
-            )
-            .background(
-                Color(
-                    .pinkBackground
-                )
-            )
-            .cornerRadius(
-                20
-            )
-            .overlay(
-                RoundedRectangle(
-                    cornerRadius: 20
-                )
-                .stroke(
-                    Color(
-                        .orangeButtomOn
-                    ),
-                    lineWidth: 4
-                )
-            )
-            .shadow(
-                radius: 10
-            )
-            .transition(
-                .opacity
-            )
-        }.ignoresSafeArea()
+        }
     }
 }
 
 #Preview {
-    PopUp(showAlert: .constant(
-        true
-    ),
-           result: .constant(
-            75.0
-           ),
-           resetAttributes: {
-    })
+    PopUp(
+        showAlert: .constant(true),
+        result: .constant(75.0),
+        resetAttributes: {}
+    )
 }
